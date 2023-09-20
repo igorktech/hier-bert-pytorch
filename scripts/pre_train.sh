5a13e392bacd968ac83b12b608eac3a05d92fe24
Igor <igoranchikkuzmin@yandex.ru>
2023-09-20 19:19:26 +0200

    minor(dev): set pre-train script params

--- scripts/pre_train.sh
+++ scripts/pre_train.sh
@@ -27,18 +27,20 @@ python3 "$PROJECT_DIR/pre_training/run_mlm.py" \
   --do_train \
   --do_eval \
   --output_dir "$OUTPUT_MODEL_DIR" \
+  --push_to_hub \
+  --hub_model_id "igorktech\$MODEL_TYPE-bert-$LAYOUT-mlm" \
   --overwrite_output_dir \
   --logging_steps 500 \
   --evaluation_strategy steps \
-  --eval_steps 5000 \
+  --eval_steps 10000 \
   --save_strategy steps \
-  --save_steps 5000 \
+  --save_steps 25000 \
   --save_total_limit 2 \
-  --max_steps 25 \
-  --learning_rate 1e-2 \
+  --max_steps 100000 \
+  --learning_rate 1e-4 \
   --trust_remote_code \
-  --per_device_train_batch_size 4 \
-  --per_device_eval_batch_size 4 \
+  --per_device_train_batch_size 64 \
+  --per_device_eval_batch_size 16 \
   --gradient_accumulation_steps 4 \
   --eval_accumulation_steps 4 \
   --lr_scheduler_type linear \
@@ -48,4 +50,6 @@ python3 "$PROJECT_DIR/pre_training/run_mlm.py" \
   --max_seq_length $MODEL_MAX_LENGTH \
   --line_by_line \
   --pad_to_max_length \
+  --report_to "wandb" \
+  --run_name $MODEL_TYPE-bert-$LAYOUT-mlm
 #  --torch_compile
